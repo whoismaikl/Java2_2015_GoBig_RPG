5ea4c6760599fef986bc5aa25d31d28201b54096
Dainis <dainis.vevers@gmail.com>
2015-10-26 23:00:41 +0200

    Modyfied User class

--- src/test/java/lv/javaguru/java2/database/jdbc/UserDAOImplTest.java
+++ src/test/java/lv/javaguru/java2/database/jdbc/UserDAOImplTest.java
@@ -25,34 +25,50 @@ public class UserDAOImplTest {
 
     @Test
     public void testCreate() throws DBException {
-        User user = createUser("F", "L");
+        User user = new User("Bob", "Wilson", "Gotcha", "U");
 
         userDAO.create(user);
 
-        User userFromDB = userDAO.getById(user.getUserId());
+        User userFromDB = userDAO.getById(user.getId());
         assertNotNull(userFromDB);
-        assertEquals(user.getUserId(), userFromDB.getUserId());
-        assertEquals(user.getFirstName(), userFromDB.getFirstName());
-        assertEquals(user.getLastName(), userFromDB.getLastName());
+        assertEquals(user.getId(), userFromDB.getId());
+        assertEquals(user.getName(), userFromDB.getName());
+        assertEquals(user.getSurname(), userFromDB.getSurname());
     }
 
     @Test
-    public void testMultipleUserCreation() throws DBException {
-        User user1 = createUser("F1", "L1");
-        User user2 = createUser("F2", "L2");
+     public void testMultipleUserCreation() throws DBException {
+        User user1 = new User("Ken", "Razor", "Gang", "U");
+        User user2 = new User("Admin", "admin", "adm", "A");
         userDAO.create(user1);
         userDAO.create(user2);
         List<User> users = userDAO.getAll();
         assertEquals(2, users.size());
     }
+    @Test
+    public void testDeleteUser() throws DBException {
+        User user1 = new User("N1", "S1", "Nik1", "U");
+        User user2 = new User("N2", "S2", "Nik2", "A");
+        User user3 = new User("N3", "S3", "Nik3", "U");
+        User user4 = new User("N4", "S4", "Nik4", "A");
+        userDAO.create(user1);
+        userDAO.create(user2);
+        userDAO.create(user3);
+        userDAO.create(user4);
+        List<User> users = userDAO.getAll();
+        assertEquals(4, users.size());
+        userDAO.delete(user2.getId());
+        users = userDAO.getAll();
+        assertEquals(3, users.size());
+    }
 
 
-
-    private User createUser(String firstName, String lastName) {
+/*
+    private User createUser(String name, String surame) {
         User user = new User();
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return user;
     }
-
+*/
 }
