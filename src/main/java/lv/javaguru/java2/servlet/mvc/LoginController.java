021bc795e2e5b80528e83b79cf3b07b6d999758a
whoismaikl <whoismaikl@gmail.com>
2015-11-10 18:52:31 +0200

    To Do: Login page

--- src/main/java/lv/javaguru/java2/servlet/mvc/LoginController.java
+++ src/main/java/lv/javaguru/java2/servlet/mvc/LoginController.java
@@ -1,12 +1,37 @@
 package lv.javaguru.java2.servlet.mvc;
 
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.jdbc.UserDAOImpl;
+
 import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by AST on 2015.11.03..
  */
 public class LoginController implements MVCController {
-    public MVCModel execute(HttpServletRequest request){
-        return  new MVCModel("Login", "/index.jsp");
+
+    public MVCModel execute(HttpServletRequest request) throws DBException {
+
+
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+
+        MVCModel model = new MVCModel("Blank","/noPage.jsp");
+        UserDAOImpl userDAO = new UserDAOImpl();
+        if (!userDAO.checkLoginData(email, password)){
+
+            //create session;
+            //Open user page;
+            model.setData("Login Success");
+            model.setViewName("/main.jsp");
+
+        } else {
+            model.setData("Name Or Password not found in Our Database");
+            model.setViewName("/index.jsp");
+        }
+
+        return model;
     }
+
+
 }
