541b95963a4c936c4af06d039c96e041d3cbb557
DainisV <dainis.vevers@gmail.com>
2016-01-13 14:05:28 +0200

    Popravki

---
+++ src/main/java/lv/javaguru/java2/servlet/controllers/AccomplishTaskControllerImpl.java
@@ -0,0 +1,43 @@
+package lv.javaguru.java2.servlet.controllers;
+
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.TaskDAO;
+import lv.javaguru.java2.domain.Task;
+import lv.javaguru.java2.domain.User;
+import lv.javaguru.java2.services.TaskService;
+import lv.javaguru.java2.servlet.controllers.controllerInterfaces.AccomplishTaskController;
+import lv.javaguru.java2.servlet.controllers.controllerInterfaces.ActiveTasksController;
+import lv.javaguru.java2.servlet.mvc.MVCModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+/**
+ * Created by AST on 2015.11.03..
+ */
+@Component
+public class AccomplishTaskControllerImpl implements AccomplishTaskController {
+
+    @Autowired
+    @Qualifier("TaskDAO_ORM")
+    private TaskDAO taskDAO;
+    @Autowired
+    private TaskService taskService;
+
+    public MVCModel execute(HttpServletRequest request) throws DBException {
+        MVCModel mvcModel = taskService.executeRequest(request);
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        List<Task> taskList = taskDAO.getAllUserTasks(user);  // tut dolzhny byt' tol'ko aktivnye
+        session.setAttribute("taskList", taskList);
+
+        return  mvcModel;
+        //return new MVCModel("Accomplish Task", "/activeTasks.jsp");
+    }
+
+}
