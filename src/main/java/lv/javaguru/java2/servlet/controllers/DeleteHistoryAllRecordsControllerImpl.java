a6bcf60c9d35d3dcc7bbd2ac0a586b9e00d801f3
DainisV <dainis.vevers@gmail.com>
2016-02-04 16:05:22 +0200

    Dobavlen: Delete HistoryRecords

---
+++ src/main/java/lv/javaguru/java2/servlet/controllers/DeleteHistoryAllRecordsControllerImpl.java
@@ -0,0 +1,47 @@
+package lv.javaguru.java2.servlet.controllers;
+
+import lv.javaguru.java2.database.DBException;
+import lv.javaguru.java2.database.HistoryRecordDAO;
+import lv.javaguru.java2.database.UserDAO;
+import lv.javaguru.java2.domain.User;
+import lv.javaguru.java2.services.ButtonFunctionService;
+import lv.javaguru.java2.services.SessionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.jws.soap.SOAPBinding;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by mike on 2015.11.03..
+ */
+@Controller
+public class DeleteHistoryAllRecordsControllerImpl {
+    @Autowired
+    @Qualifier("HistoryDAO_ORM")
+    private HistoryRecordDAO historyRecordDAO;
+    @Autowired
+    SessionService sessionService;
+
+    @RequestMapping(value = "/deleteAllHistoryRecords", method = {RequestMethod.POST})
+    public ModelAndView execute(HttpServletRequest request) throws DBException, IOException {
+
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+        Long userId = user.getId();
+
+        historyRecordDAO.deleteAllHistoryRecordByUserId(userId);
+
+        sessionService.updateSessionVariables(request);
+
+        return  new ModelAndView("redirect:/taskHistory");
+    }
+}
--- src/main/java/lv/javaguru/java2/servlet/controllers/DeleteHistoryRecordControllerImpl.java
+++ src/main/java/lv/javaguru/java2/servlet/controllers/DeleteHistoryRecordControllerImpl.java
@@ -2,19 +2,16 @@ package lv.javaguru.java2.servlet.controllers;
 
 import lv.javaguru.java2.database.DBException;
 import lv.javaguru.java2.database.HistoryRecordDAO;
-import lv.javaguru.java2.database.UserDAO;
 import lv.javaguru.java2.domain.User;
 import lv.javaguru.java2.services.ButtonFunctionService;
 import lv.javaguru.java2.services.SessionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.jws.soap.SOAPBinding;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
@@ -24,25 +21,25 @@ import java.util.List;
  * Created by mike on 2015.11.03..
  */
 @Controller
-public class DeleteHistoryRecordsControllerImpl {
+public class DeleteHistoryRecordControllerImpl {
     @Autowired
     @Qualifier("HistoryDAO_ORM")
     private HistoryRecordDAO historyRecordDAO;
     @Autowired
     SessionService sessionService;
+    @Autowired
+    private ButtonFunctionService buttonFunctionService;
 
-    @RequestMapping(value = "/deleteAllHistoryRecords", method = {RequestMethod.POST})
+    @RequestMapping(value = "/deleteHistoryRecord", method = {RequestMethod.POST})
     public ModelAndView execute(HttpServletRequest request) throws DBException, IOException {
 
-        HttpSession session = request.getSession();
-        User user = (User) session.getAttribute("user");
-        Long userId = user.getId();
-
-        historyRecordDAO.deleteAllHistoryRecordByUserId(userId);
-
-        sessionService.updateSessionVariables(request);
-
-
-        return new ModelAndView("/taskHistory.jsp", "model", "All History Record Deleted");
+        String taskString = buttonFunctionService.getButtonName(request);
+        if (!taskString.isEmpty()) {
+            Long recordId = buttonFunctionService.getId(taskString);
+            historyRecordDAO.deleteHistoryRecordById(recordId);
+            sessionService.updateSessionVariables(request);
+            return new ModelAndView("redirect:/taskHistory");
+        }
+        return  new ModelAndView("/taskHistory.jsp", "model", "Delete Error");
     }
 }
